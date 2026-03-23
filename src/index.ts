7c69073e2794856d85cfc787e208d094051828d6
ohshuxbarbie <koolnye@gmail.com>
2026-03-23 11:44:09 +0800

    feat: api implementation for posts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import postRoutes from './routes/post.routes.js'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route("/", postRoutes);
 
 serve({
   fetch: app.fetch,
   port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+});
+
+console.log("Server starting...")
